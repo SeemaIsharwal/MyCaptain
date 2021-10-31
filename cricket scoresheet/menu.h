a7baed2697803470a2b4d98168e84f460e88a410
SeemaIsharwal <92375687+SeemaIsharwal@users.noreply.github.com>
2021-10-31 15:02:07 +0530

    .h header file for actions to be performed

---
+++ cricket scoresheet/menu.h
@@ -0,0 +1,10 @@
+void menu()
+{
+
+system("color 9");
+    printf("\n\n\t\t\t\t\tCHOOSE YOUR ACTION:\t\t\t\n");
+    printf("\n\n\n\t\t\t\t1.Open New Scoresheet:\n");
+    printf("\t\t\t\t2.View Already Existing Scoresheet:\n");
+    printf("\t\t\t\t3.Exit The System:\n\t\t");
+}
+
